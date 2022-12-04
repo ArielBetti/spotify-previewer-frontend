25d7d0f4c4e980d09b2c395630968283557f2eca
ArielBetti <ariel.betti@gmail.com>
2022-12-03 23:54:59 -0300

    feat(login-page): update login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -14,8 +14,17 @@ const Login = () => {
   }, [user]);
 
   return (
-    <div>
-      <a href="http://localhost:8080/login">Login on Spotify</a>
+    <div className="flex flex-col gap-3">
+      <h1 className="text-3xl font-bold">Welcome!</h1>
+      <p>This application is a spotify music preview</p>
+      <div className="pt-3">
+        <a
+          className="text-white bg-spotify-green p-3 rounded-sm shadow-lg font-semibold w-auto hover:bg-spotify-green-200 transition-colors"
+          href="http://localhost:8080/login"
+        >
+          Login on Spotify
+        </a>
+      </div>
     </div>
   );
 };
