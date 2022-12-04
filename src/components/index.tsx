9e0e649f6da307b3d993ff1a4ff4f55552f0710d
ArielBetti <ariel.betti@gmail.com>
2022-12-03 23:51:28 -0300

    feat(components): export atomic components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,32 @@
-import Button from "./Button";
-import Input from "./Input";
+// atoms: components
+import Button from "./Atoms/Button";
+import CardItem from "./Atoms/CardItem";
+import Header from "./Molecules/Header";
+import Hero from "./Atoms/Hero";
+import InlineLoader from "./Atoms/InlineLoader";
+import Input from "./Atoms/Input";
+import ThemeToggle from "./Atoms/ThemeToggle";
+import UserMenu from "./Atoms/UserMenu";
 
-export { Button, Input };
+// molecules: components
+import DraggableItems from "./Molecules/DraggableItems";
+import ItemsSection from "./Molecules/ItemsSection";
+import Search from "./Molecules/Search";
+import TapumeLoader from "./Molecules/TapumeLoader";
+import TrackList from "./Molecules/TrackList";
+// ::
+export {
+  Button,
+  CardItem,
+  DraggableItems,
+  Header,
+  Hero,
+  InlineLoader,
+  Input,
+  ItemsSection,
+  Search,
+  TapumeLoader,
+  ThemeToggle,
+  TrackList,
+  UserMenu,
+};
