74134f8960de52b2312a5bccf07d4cfeb3da4c65
ArielBetti <ariel.betti@gmail.com>
2022-12-03 23:54:46 -0300

    feat(home): update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,66 @@
-import { useRecoilValue, useRecoilValueLoadable } from "recoil";
-import { Button, Input } from "../../components";
-import { atomUser } from "../../store/atoms";
-import { selectorGetUser } from "../../store/selector";
+import { useEffect, useMemo } from "react";
+import { useRecoilValueLoadable, useSetRecoilState } from "recoil";
 
+// atomic: components
+import {
+  InlineLoader,
+  ItemsSection,
+  Search,
+  TrackList,
+} from "../../components";
+
+// recoil: atoms
+import { atomAlbums, atomArtists, atomTracks } from "../../store/atoms";
+
+// recoil: selectors
+import { selectorGetSearch, selectorGetUser } from "../../store/selector";
+
+// ::
 const Home = () => {
-  const user = useRecoilValue(atomUser);
+  // recoil: states
+  const setArtists = useSetRecoilState(atomArtists);
+  const setAlbums = useSetRecoilState(atomAlbums);
+  const setTracks = useSetRecoilState(atomTracks);
 
+  // recoil: loadable
+  const getSearchLodable = useRecoilValueLoadable(selectorGetSearch);
   useRecoilValueLoadable(selectorGetUser);
+
+  // memo: states
+  const displayInlineLoader = useMemo(() => {
+    // includes all possible loadings in this page
+    if (getSearchLodable.state === "loading") {
+      return true;
+    }
+
+    return false;
+  }, [getSearchLodable]);
+
+  useEffect(() => {
+    if (
+      getSearchLodable.state === "hasValue" &&
+      getSearchLodable.contents !== undefined
+    ) {
+      if (getSearchLodable.contents?.albums?.items?.length > 0) {
+        setAlbums(getSearchLodable.contents.albums.items);
+      }
+      if (getSearchLodable.contents?.artists?.items?.length > 0) {
+        setArtists(getSearchLodable.contents.artists.items);
+      }
+      if (getSearchLodable.contents?.tracks?.length > 0) {
+        setTracks(getSearchLodable.contents.tracks);
+      }
+    }
+  }, [getSearchLodable.state, getSearchLodable.contents]);
 
   return (
     <div>
-      <div className="flex gap-2 pt-2 md:max-w-sm max-w-full">
-        <Input placeholder="O que você quer ouvir?" />
-        <Button>Ok</Button>
+      <Search />
+      <InlineLoader open={displayInlineLoader} text="Carregando..." />
+      <div className="py-10">
+        <ItemsSection />
       </div>
+      <TrackList />
     </div>
   );
 };
