199f26168286d2641b7dff99518e1be4265459a9
ArielBetti <ariel.betti@gmail.com>
2022-12-03 23:52:06 -0300

    feat(recoil): create new states and singletons

--- src/store/atoms/index.ts
+++ src/store/atoms/index.ts
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
-import { TUser } from "../../interfaces";
+import { TCurrentTrack, TSpotifySearchAlbumsItem, TSpotifySearchArtistsItem, TSpotifySearchTracksItem, TUser } from "../../interfaces";
 import { localStorageEffect } from "../../utils/localStorageEffect";
+
+export const atomTheme = atom<'dark' | 'light'>({
+  key: "atomTheme",
+  default: "dark",
+  effects: [localStorageEffect("atomTheme")],
+});
 
 export const atomToken = atom<string>({
   key: "atomToken",
@@ -13,3 +19,28 @@ export const atomUser = atom<TUser>({
   default: undefined,
   effects: [localStorageEffect("atomUser")],
 });
+
+export const atomSearch = atom<string>({
+  key: "atomSearch",
+  default: '',
+});
+
+export const atomAlbums = atom<TSpotifySearchAlbumsItem[]>({
+  key: "atomAlbums",
+  default: [],
+});
+
+export const atomArtists = atom<TSpotifySearchArtistsItem[]>({
+  key: "atomArtists",
+  default: [],
+});
+
+export const atomTracks = atom<TSpotifySearchTracksItem[]>({
+  key: "atomTracks",
+  default: [],
+});
+
+export const atomCurrentTrack = atom<TCurrentTrack>({
+  key: 'atomCurrentTrack',
+  default: undefined,
+})
--- src/store/selector/index.ts
+++ src/store/selector/index.ts
@@ -1,6 +1,13 @@
-import { selector } from "recoil";
+import { selector, selectorFamily } from "recoil";
 import { requester } from "../../api/requester";
-import { atomToken } from "../atoms";
+import {
+  TSpotifyAlbumTracks,
+  TSpotifyArtistTopTracks,
+  TSpotifySearch,
+  TSpotifySearchTracks,
+  TSpotifySearchTracksItem,
+} from "../../interfaces";
+import { atomSearch, atomToken } from "../atoms";
 
 export const selectorGetUser = selector({
   key: "selectorGetUser",
@@ -15,4 +22,100 @@ export const selectorGetUser = selector({
 
     return data;
   },
+});
+
+export const selectorGetSearch = selector({
+  key: "selectorGetSearch",
+  get: async ({ get }): Promise<TSpotifySearch | undefined> => {
+    const token = get(atomToken);
+    const search = get(atomSearch);
+
+    if (!token || !search) return;
+
+    const { data } = await requester({
+      Authorization: token,
+    }).get(`search?q=${search}&type=track%2Cartist%2Calbum&market=BR&limit=35`);
+
+    Promise.resolve(data);
+
+    if (data?.tracks?.items?.length > 0) {
+      const searchTracksId = data?.tracks?.items.map(
+        (track: TSpotifySearchTracksItem) => track.id
+      );
+
+      const trackSearch = await requester({
+        Authorization: token,
+      }).get(`tracks?market=BR&ids=${searchTracksId}`);
+
+      Promise.resolve(trackSearch);
+
+      const tracks = trackSearch.data;
+
+      return { ...data, ...tracks };
+    }
+
+    return data;
+  },
+});
+
+export const selectorGetArtistTracks = selectorFamily({
+  key: "selectorGetArtistTracks",
+  get:
+    (id: string) =>
+    async ({ get }): Promise<TSpotifyArtistTopTracks | undefined> => {
+      const token = get(atomToken);
+
+      if (!token) return;
+
+      const { data } = await requester({
+        Authorization: token,
+      }).get(`artists/${id}`);
+
+      Promise.resolve(data);
+
+      if (data?.name) {
+        const topTracks = await requester({
+          Authorization: token,
+        }).get(`artists/${id}/top-tracks?market=BR`);
+
+        Promise.resolve(topTracks);
+
+        const tracks = topTracks?.data || null;
+
+        return { artist: { ...data }, ...tracks };
+      }
+
+      return data;
+    },
+});
+
+export const selectorGetAlbum = selectorFamily({
+  key: "selectorGetAlbum",
+  get:
+    (id: string) =>
+    async ({ get }): Promise<TSpotifyAlbumTracks | undefined> => {
+      const token = get(atomToken);
+
+      if (!token) return;
+
+      const { data } = await requester({
+        Authorization: token,
+      }).get(`albums/${id}`);
+
+      Promise.resolve(data);
+
+      if (data?.name) {
+        const topTracks = await requester({
+          Authorization: token,
+        }).get(`albums/${id}/tracks?market=BR`);
+
+        Promise.resolve(topTracks);
+
+        const tracks = topTracks?.data?.items || null;
+
+        return { album: { ...data }, tracks: { ...tracks } };
+      }
+
+      return data;
+    },
 });
